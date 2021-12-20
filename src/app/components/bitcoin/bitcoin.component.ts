4a0b53f4e1936761bf433be38afa8d907a21f56a
Miguel Angel Larios Campos <mlariosc7@hotmail.com>
2021-12-20 07:44:48 -0600

    Recurrent API Request

--- src/app/components/bitcoin/bitcoin.component.ts
+++ src/app/components/bitcoin/bitcoin.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RestService } from 'src/app/services/rest.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { apiResponse } from 'src/app/models/apiResponse';
+import { Observable,interval } from 'rxjs';
 
 @Component({
   selector: 'app-bitcoin',
   templateUrl: './bitcoin.component.html',
   styleUrls: ['./bitcoin.component.css']
 })
-export class BitcoinComponent implements OnInit {
-list: any[] =[];
+export class BitcoinComponent implements OnInit, OnDestroy {
+  list: any[] = [];
+  count = 0;
+  obs:any;
   constructor(
 
     private rest: RestService,
@@ -19,14 +22,22 @@ list: any[] =[];
     private route: ActivatedRoute,
     private matSnackBar: MatSnackBar
   ) { }
+  ngOnDestroy(): void {
+    this.obs.unsubscribe();
+  }
 
   ngOnInit(): void {
-    this.loadCurrencies();
+   this.obs = interval(5000).subscribe(x => {
+      this.count += 1;
+      this.loadCurrencies();
+    })
+    //this.loadCurrencies();
   }
 
   loadCurrencies() {
     this.rest.getCurrencys().subscribe((resp: any) => {
       console.log(resp);
+     this.list = resp.data;
 
     });
   }
